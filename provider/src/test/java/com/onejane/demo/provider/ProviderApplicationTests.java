7e16cc5224c19a92fb91b890a3bea02efea0e168
onejane <wangjian@jddmoto.com>
2021-03-12 20:31:22 +0800

    阿里云整合上传图片

--- provider/src/test/java/com/onejane/demo/provider/ProviderApplicationTests.java
+++ provider/src/test/java/com/onejane/demo/provider/ProviderApplicationTests.java
@@ -1,13 +1,31 @@
 package com.onejane.demo.provider;
 
+import com.aliyun.oss.OSSClient;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class ProviderApplicationTests {
+public class ProviderApplicationTests {
+
+    @Autowired
+    OSSClient ossClient;
 
     @Test
-    void contextLoads() {
+    public void testUpload() throws FileNotFoundException {
+        // 上传文件流。
+        InputStream inputStream = new FileInputStream("E:\\picture\\zz.jpg");
+        ossClient.putObject("onejane-opencv", "zz.jpg", inputStream);
+        // 关闭OSSClient。
+        ossClient.shutdown();
+        System.out.println("上传完成...");
     }
-
 }
